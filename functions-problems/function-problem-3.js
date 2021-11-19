30c518dad011ac3950ad1e1c958e5b8c0820ce3a
Michael <michaelmouet@gmail.com>
2021-11-19 12:20:37 -0800

    Third Commit: added function-problem-3.

---
+++ functions-problems/function-problem-3.js
@@ -0,0 +1,22 @@
+/* INSTRUCTIONS: Write a JavaScript function that accepts two arguments, 
+a string and a letter and the function will count the number of occurrences of the specified letter within the string. 
+*/
+
+//FIRST Attempt
+//After reviewing the solution for the previous problem, I saw that this one was exteremely similar. 
+//All it needed was another input for the user. 
+
+const checkForLetter = (userString, userLetter) => {
+  userString = userString.toLowerCase();
+  userLetter = userLetter.toLowerCase();
+ 
+  let letterCounter = 0;
+ 
+  for (let i = 0; i < userString.length; i++)
+    if (userLetter.includes(userString[i])) {
+      letterCounter++
+    }
+  return letterCounter
+};
+
+console.log(checkForLetter("seven seven seven", "v"));
