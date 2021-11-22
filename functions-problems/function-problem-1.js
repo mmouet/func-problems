bf91215dd5db54a0eaec70b1c6530f937950824b
Michael <michaelmouet@gmail.com>
2021-11-22 05:33:10 -0800

    Sixth Commit: Revised function-problem-1

--- functions-problems/function-problem-1.js
+++ functions-problems/function-problem-1.js
@@ -3,6 +3,7 @@ and find the longest word within the string. */
 
 //I felt so out of practice that I had to immediately look at the solution. 
 
+/*
 let myString = "This is my string example sentence";
 
 const findLongestWord = userString => {
@@ -15,6 +16,25 @@ const findLongestWord = userString => {
       }
   }
   return compareString
+};
+
+console.log(findLongestWord(myString));
+*/
+
+
+//SECOND Attempt
+let myString = "This is my string example sentence";
+
+const findLongestWord = userString => {
+  const splitString = userString.split(" ");
+  let longestWord = "";
+  
+  for (let i = 0; i < splitString.length; i++) {
+      if (splitString[i].length > longestWord.length) {
+        longestWord = splitString[i]
+      }
+  }
+  return longestWord
 };
 
 console.log(findLongestWord(myString));
