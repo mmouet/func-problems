dd66456805c296f90537a79188bd06dabcbc11fd
Michael <michaelmouet@gmail.com>
2021-11-22 14:18:20 -0800

    Ninth Commit: Revised function-problem-4

--- functions-problems/function-problem-4.js
+++ functions-problems/function-problem-4.js
@@ -47,11 +47,30 @@ const myFunction = () => {
 //THIRD Attempt
 //I looked at the example solution and saw that I missed the instruction to begin the loop at 1, so I changed the loop starting point. 
 //I renamed the function to more accurately reflect it. 
-
+/*
 const fizzBuzzFunc = () => {
   for (let i = 1; i <= 100; i++) {
     console.log(i)
     if (i % 3 + i % 5 === 0) {
+      console.log("FizzBuzz")
+    } else if (i % 3 === 0) {
+      console.log("Fizz")
+    } else if (i % 5 === 0) {
+      console.log("Buzz")
+    }
+  }
+};
+
+console.log(fizzBuzzFunc())
+*/
+
+
+//FOURTH Attempt
+//Changed ++ in the "FizzBuzz" if statement to include &&.
+const fizzBuzzFunc = () => {
+  for (let i = 1; i <= 100; i++) {
+    console.log(i)
+    if (i % 3 === 0 && i % 5 === 0) {
       console.log("FizzBuzz")
     } else if (i % 3 === 0) {
       console.log("Fizz")
