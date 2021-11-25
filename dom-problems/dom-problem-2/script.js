d1b8467c69d3dc003cdf593fa71f5da9f9fde766
Michael <michaelmouet@gmail.com>
2021-11-25 08:30:00 -0800

    11th Commit: Created a folder for each dom problem. Renamed files to index.html and script.js

---
+++ dom-problems/dom-problem-2/script.js
@@ -0,0 +1,25 @@
+/* INSTRUCTIONS: 
+Write a JavaScript function to get the HREF attribute of the link when you click the button
+*/
+
+// PLANNING:
+// Create a function
+// Target the html button with a variable
+// Use the variable to get the HREF attribute
+
+
+// FIRST ATTEMPT
+// const getHREF = () => {
+// 	const clickButton = document.getElementById("link");
+// 	clickButton.getAttribute("href");
+// };
+
+
+
+// SECOND ATTEMPT
+// Was confused by what the instructions wanted the function to do until sample solution was viewed
+// Learned code was missing .innerHTML to make the P element show the hyperlink
+const getHREF = () => {
+	const clickButton = document.getElementById("link").getAttribute("href");
+	document.getElementById("p1").innerHTML = clickButton;
+};
