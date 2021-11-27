3bb96994144891063a9f7e9918323f781d064c8c
Michael <michaelmouet@gmail.com>
2021-11-27 11:10:49 -0800

    13th commit: Revised dom-problem-3 index.html and script.js files

--- dom-problems/dom-problem-3/script.js
+++ dom-problems/dom-problem-3/script.js
@@ -2,11 +2,26 @@
 // Write a JavaScript function to add rows to the table when you click on the button. 
 
 // PLANNING
-// 
+// Create function 
 
 
-
+// FIRST ATTEMPT
+/*
 const addRow = () => {
 	const clickButton = document.getElementById("table1");
 	clickButton.insertRow(0).innerHTML = "New Row"
+};
+*/
+
+
+// SECOND ATTEMPT
+// Rename variables
+// Need to add a .insertCell method to give the rows table data 
+// Remove .innerHTML from addRow variable so .innerHTML works on addCell variable
+const addRow = () => {
+	const table = document.getElementById("table1");
+	const addRow = table.insertRow(0);
+	const addCell1 = addRow.insertCell(0).innerHTML = "New Cell 1";
+	const addCell2 = addRow.insertCell(1).innerHTML = "New Cell 2";
+
 };
