1c29f052b4e5cca0f6fa71bee874f89436b727d5
Michael <michaelmouet@gmail.com>
2021-11-30 19:02:49 -0800

    17th Commit: Added array-problem-3.js

---
+++ array-problems/array-problem-3.js
@@ -0,0 +1,13 @@
+// INSTRUCTIONS 
+// Define a function that takes in an array and returns the sorted version of that array.
+
+// PLANNING: 
+// Create function and example array.
+
+let numArray = [1,9,2,8,3,5,4,7,6];
+
+const sortNum = arrayInput => {
+	return arrayInput.sort()
+};
+
+console.log(sortNum(numArray));
