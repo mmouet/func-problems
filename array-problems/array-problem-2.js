8fb928357483a8afe4fbc553d78d53a74f7d1b99
Michael <michaelmouet@gmail.com>
2021-11-30 19:00:43 -0800

    16th Commit: Added array-problem-2.js

---
+++ array-problems/array-problem-2.js
@@ -0,0 +1,14 @@
+// INSTRUCTIONS 
+// Define a function that takes in an array of elements and joins them together into a string.
+// Do NOT use the "toString" method.
+
+// PLANNING: 
+// Create function and example array. 
+
+let citiesArray = ["Tokyo", "London", "Paris", "Prague", "Chicago"];
+
+const joinArray = arrayInput => {
+	return arrayInput.join()
+};
+
+console.log(joinArray(citiesArray));
