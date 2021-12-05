96e1e767b5351ba185066983af928dc3879b7842
Michael <michaelmouet@gmail.com>
2021-12-05 12:46:04 -0800

    23rd Commit: Added object-problem-3.js

---
+++ object-problems/object-problem-3.js
@@ -0,0 +1,16 @@
+// INSTRUCTIONS: 
+// Create an object with three items in it, then write a JavaScript function that accepts an object 
+// and tells you the length (tells you how many items are in it) of the object.
+
+
+let car = {
+	"Engine": "On",
+	"Oil Level": "Low",
+	"Check Engine Light": "On"
+};
+
+const objectLength = objectName => {
+      return Object.keys(objectName).length
+};
+
+console.log(objectLength(car))
