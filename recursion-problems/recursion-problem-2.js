e6743d2c03401895fd65dccdccae6aeeba17e56a
Michael <michaelmouet@gmail.com>
2021-12-06 23:33:27 -0800

    29th Commit: added recursion-problem-2.js

---
+++ recursion-problems/recursion-problem-2.js
@@ -0,0 +1,30 @@
+// INSTRUCTIONS
+// Write a JavaScript function to compute the sum of an array of integers.
+
+// PLANNING: 
+// Use for loop to iterate array and add each element
+
+// Without recursion
+let arr = [1,2,3,4,5,6,7,8,9,10]
+const sumArray = x => {
+	let sum = 0
+	for (let i = 0; i <= x.length; i++) {
+		sum += i
+	};
+	return sum
+};
+console.log(sumArray(arr))
+
+
+// Function with recursion
+let arr = [1,2,3,4,5,6,7,8,9,10]
+
+const recursionSum = x => {
+	if (x.length === 0) {
+    	return 0
+	} else {
+    	return x[0] + recursionSum(x.slice(1)) 
+	};
+};
+
+console.log(recursionSum(arr))
